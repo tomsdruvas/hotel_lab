cefbf224a2b1ef33c7e302645a1bf4e34ff05531
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-01-19 12:09:31 +0000

    commit after adding routers

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router')
+
+app.use(express.json())
+MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
+.then((client) => {
+    const db = client.db('hotel_bookings')
+    const bookingsCollection = db.collection('bookings')
+    const bookingsRouter = createRouter(bookingsCollection)
+    app.use('/api/bookings', bookingsRouter)
+})
+.catch(console.error)
+
+
+
+app.listen(5000, () => {
+    console.log("App running on 5000")
+})
